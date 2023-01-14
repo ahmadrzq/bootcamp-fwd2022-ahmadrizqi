707f8eeda46683ba4e4c198bc23b748f693d738c
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-14 21:14:25 +0700

    Update setting button to dashboard in header frontsite

--- resources/views/components/frontsite/header.blade.php
+++ resources/views/components/frontsite/header.blade.php
@@ -82,8 +82,8 @@
                             role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <a href="#" class="block px-4 py-2 text-sm text-[#1E2B4F] hover:bg-gray-100"
                                 role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-[#1E2B4F] hover:bg-gray-100"
-                                role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+                            <a href="{{route('backsite.dashboard.index')}}" class="block px-4 py-2 text-sm text-[#1E2B4F] hover:bg-gray-100"
+                                role="menuitem" tabindex="-1" id="user-menu-item-1">Dashboard</a>
                             <a href="{{ route('logout') }}"
                                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
                                 class="block px-4 py-2 text-sm text-[#1E2B4F] hover:bg-gray-100" role="menuitem"
