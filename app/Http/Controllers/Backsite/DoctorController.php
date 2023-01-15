d62b852ca20aa4d998638cd5050f89dff1b61a26
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-01-15 07:27:09 +0700

    Adding doctor controller and function

---
+++ app/Http/Controllers/Backsite/DoctorController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Http\Controllers\Backsite;
+
+use App\Http\Controllers\Controller;
+
+// use library here
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+// request
+use App\Http\Requests\Doctor\StoreDoctorRequest;
+use App\Http\Requests\Doctor\UpdateDoctorRequest;
+
+// use everything here
+use Gate;
+use Auth;
+use File;
+
+// use model here
+use App\Models\User;
+use App\Models\Operational\Doctor;
+use App\Models\MasterData\Specialist;
+
+// thirdparty package
+
+class DoctorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // for table grid
+        $doctor = Doctor::orderBy('created_at', 'desc')->get();
+
+        //for selet2 = ascending a to z
+        $specialist = Specialist::orderBy('name', 'asc')->get();
+
+        return view('pages.backsite.operational.doctor.index', compact('doctor', 'specialist'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return abort(404);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreDoctorRequest $request)
+    {
+        // get all request from frontsite
+        $data = $request->all();
+
+        //store data to database
+        $doctor = Doctor::create($data);
+
+        alert()->success('Success Message', 'Doctor has been created successfully!');
+        return redirect()->route('backsite.doctor.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Doctor $doctor)
+    {
+        return view('pages.backsite.operational.doctor.show', compact('doctor'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Doctor $doctor)
+    {
+        //for selet2 = ascending a to z
+        $specialist = Specialist::orderBy('name', 'asc')->get();
+
+        return view('pages.backsite.operational.doctor.edit', compact('doctor', 'specialist'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateDoctorRequest $request, Doctor $doctor)
+    {
+        // get all request from frontsite
+        $data = $request->all();
+
+        //store data to database
+        $doctor->update($data);
+
+        alert()->success('Success Message', 'Doctor has been updated successfully!');
+        return redirect()->route('backsite.doctor.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return abort(404);
+    }
+}
